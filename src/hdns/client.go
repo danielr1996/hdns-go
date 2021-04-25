c943d02195efb1e1e750a0bb09d76ebc8ad2dcd4
Daniel Richter <danielrichter@posteo.de>
2021-04-25 19:56:27 +0200

    feat: implement add and get records

--- src/hdns/client.go
+++ src/hdns/client.go
@@ -1,8 +1,10 @@
 package hdns
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -31,8 +33,15 @@ func (c Client) WithEndpoint(endpoint string) Client {
 	return c
 }
 
-type Response struct {
+type ZoneResponse struct {
 	Zones []map[string]interface{} `json:"zones"`
+}
+
+type RecordsResponse struct {
+	Records []map[string]interface{} `json:"records"`
+}
+type RecordResponse struct {
+	Record map[string]string `json:"record"`
 }
 
 func (c Client) Zones() []map[string]interface{} {
@@ -45,7 +54,7 @@ func (c Client) Zones() []map[string]interface{} {
 	if err != nil {
 		fmt.Printf(err.Error())
 	}
-	var res Response
+	var res ZoneResponse
 	err = json.NewDecoder(resp.Body).Decode(&res)
 	if err != nil {
 		fmt.Printf(err.Error())
@@ -54,8 +63,8 @@ func (c Client) Zones() []map[string]interface{} {
 	return res.Zones
 }
 
-func (c Client) AddRecord() []map[string]interface{} {
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/zones", c.endpoint), nil)
+func (c Client) ZonesByName(name string) []map[string]interface{} {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/zones?name=%s", c.endpoint, name), nil)
 	if err != nil {
 		fmt.Printf(err.Error())
 	}
@@ -64,11 +73,57 @@ func (c Client) AddRecord() []map[string]interface{} {
 	if err != nil {
 		fmt.Printf(err.Error())
 	}
-	var res Response
+	var res ZoneResponse
 	err = json.NewDecoder(resp.Body).Decode(&res)
 	if err != nil {
 		fmt.Printf(err.Error())
 	}
 
 	return res.Zones
+}
+
+func (c Client) Records() []map[string]interface{} {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/records", c.endpoint), nil)
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+	req.Header.Add("Auth-API-Token", c.token)
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+	var res RecordsResponse
+	err = json.NewDecoder(resp.Body).Decode(&res)
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+
+	return res.Records
+}
+
+//recordType A, AAA, NS, MX, CNAME, RP, TXT, SOA, HINFO, SRV, DANE, TLSA, DS, CAA
+func (c Client) AddRecord(name string, recordType string, value string, zone_id string) map[string]string {
+	values := map[string]string{"name": name, "type": recordType, "value": value, "zone_id": zone_id}
+	json_data, err := json.Marshal(values)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/records", c.endpoint), bytes.NewBuffer(json_data))
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+	req.Header.Add("Auth-API-Token", c.token)
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+	var res RecordResponse
+	err = json.NewDecoder(resp.Body).Decode(&res)
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+
+	return res.Record
 }
