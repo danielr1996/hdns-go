73405b34d8efa091445a8eb7e5b07e844d64395e
Daniel Richter <danielrichter@posteo.de>
2021-04-25 23:41:08 +0200

    refactoring: split client into separate files and return proper structs
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/danielr1996/hdns-go
 
 go 1.15
+
+require github.com/dghubble/sling v1.3.0
--- src/hdns/client.go
+++ src/hdns/client.go
@@ -1,15 +1,11 @@
 package hdns
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"log"
+	"github.com/dghubble/sling"
 	"net/http"
 )
 
-const Endpoint = "https://dns.hetzner.com/api/v1"
+const Endpoint = "https://dns.hetzner.com/api/v1/"
 
 type Client struct {
 	token      string
@@ -20,134 +16,23 @@ type Client struct {
 func NewClient() *Client {
 	c := new(Client)
 	c.httpClient = http.Client{}
-	c.endpoint = "https://dns.hetzner.com/api/v1"
+	c.endpoint = Endpoint
 	return c
 }
 
-func (c Client) WithToken(token string) Client {
+func (c *Client) WithToken(token string) *Client {
 	c.token = token
 	return c
 }
 
-func (c Client) WithEndpoint(endpoint string) Client {
+func (c *Client) WithEndpoint(endpoint string) *Client {
 	c.endpoint = endpoint
 	return c
 }
 
-type ZoneResponse struct {
-	Zones []map[string]interface{} `json:"zones"`
-}
-
-type RecordsResponse struct {
-	Records []map[string]interface{} `json:"records"`
-}
-type RecordResponse struct {
-	Record map[string]string `json:"record"`
-}
-
-func (c Client) Zones() []map[string]interface{} {
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/zones", c.endpoint), nil)
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-	req.Header.Add("Auth-API-Token", c.token)
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-	var res ZoneResponse
-	err = json.NewDecoder(resp.Body).Decode(&res)
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-
-	return res.Zones
-}
-
-func (c Client) ZonesByName(name string) []map[string]interface{} {
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/zones?name=%s", c.endpoint, name), nil)
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-	req.Header.Add("Auth-API-Token", c.token)
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-	var res ZoneResponse
-	err = json.NewDecoder(resp.Body).Decode(&res)
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-
-	return res.Zones
-}
-
-func (c Client) ZoneByName(name string) (map[string]interface{}, error) {
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/zones?name=%s", c.endpoint, name), nil)
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-	req.Header.Add("Auth-API-Token", c.token)
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-	var res ZoneResponse
-	err = json.NewDecoder(resp.Body).Decode(&res)
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-
-
-	if len(res.Zones) != 1 {
-		return nil, errors.New("No zones with name "+name+" found")
-	}
-	return res.Zones[0], nil
-}
-
-func (c Client) Records() []map[string]interface{} {
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/records", c.endpoint), nil)
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-	req.Header.Add("Auth-API-Token", c.token)
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-	var res RecordsResponse
-	err = json.NewDecoder(resp.Body).Decode(&res)
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-
-	return res.Records
-}
-
-//recordType A, AAA, NS, MX, CNAME, RP, TXT, SOA, HINFO, SRV, DANE, TLSA, DS, CAA
-func (c Client) AddRecord(name string, recordType string, value string, zone_id string) map[string]string {
-	values := map[string]string{"name": name, "type": recordType, "value": value, "zone_id": zone_id}
-	json_data, err := json.Marshal(values)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	req, err := http.NewRequest("POST", fmt.Sprintf("%s/records", c.endpoint), bytes.NewBuffer(json_data))
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-	req.Header.Add("Auth-API-Token", c.token)
-	req.Header.Add("Content-Type", "application/json")
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-	var res RecordResponse
-	err = json.NewDecoder(resp.Body).Decode(&res)
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-
-	return res.Record
+func (c *Client) BaseApi() *sling.Sling {
+	return sling.
+		New().
+		Base(c.endpoint).
+		Set("Auth-API-Token", c.token)
 }
